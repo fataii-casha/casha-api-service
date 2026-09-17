39b5e02f8b2ba26e92050d27e35b1d047e1bc91f
fataii-casha <fatai@casha.ng>
2026-09-17 17:13:56 +0100

    fix: server envirnonment fix

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { getHealthReport } from './health.service';
 
 export async function check(res: Response) {
